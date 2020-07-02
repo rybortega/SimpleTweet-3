eff227000efb06d444f789dcc469c45cc5318167
eobbad <35933488+eobbad@users.noreply.github.com>
2020-07-02 12:12:23 -0700

    Added the compose tweet screen setup+transition

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -75,7 +76,8 @@ public class TimelineActivity extends AppCompatActivity {
 
         if (item.getItemId() == R.id.compose) {
 
-            Toast.makeText(this,"Compose", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
